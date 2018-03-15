138ff7adf3b0de47a322df5ef28181b55382ec69
Holo25 <lorenzohorv@gmail.com>
2018-03-15 19:06:19 +0100

    Routes and middleware skeleton

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ var path = require('path');
 /
 /user
 /user/edit
-/user/create
 
 /login
 /logout
 /register
-/recoverpassword
+/recover
+/admin
 
 /auctions
 /auctions/:auctionid/bid
@@ -21,12 +21,105 @@ var path = require('path');
 var userLoggedIn = true;
 
 var authUser = function (req, res, next) {
-    //Visszakuld ha nem vagyunk  bejelentkezve
+    //Visszaküld ha nem vagyunk  bejelentkezve
     if (userLoggedIn) { next(); }
+    else res.redirect('/login');
+}
+
+var reverseAuthUser = function (req, res, next) {
+    //Visszaküld ha be vagyunk  jelentkezve
+    if (!userLoggedIn) { next(); }
     else res.redirect('/');
+}
+var getUser = function (req, res, next) {
+    //megkeresi a jelenleg loginolt usert
+}
+
+var editUser = function (req, res, next) {
+    //A változásokat elmenti a user profiljában 
+}
+
+var addUser = function (req, res, next) {
+    //Új usert készít 
+}
+
+var validateUser = function (req, res, next) {
+    //Megnézi hogy ok a megadott bejelentkezési adatok
+}
+
+var logoutUser = function (req, res, next) {
+    //Kijelentkezteti a usert
+}
+
+var checkUserRegistration = function (req, res, next) {
+    //Megnézi hogy jók-e a regisztráló felhasználó adatai
+}
+
+var recoverPassword = function (req, res, next) {
+    //Elfelejtett jelszó middleware
+}
+
+var checkAdmin = function (req, res, next) {
+    //Megnézi hogy admin-e a felhasználó
+}
+var renderMW = function (req, res, next) {
+    //Rendereli az adott oldalt
+}
+
+var getAuctionList = function (req, res, next) {
+    //Visszaadja az árverések listáját(aka főoldal tartalmát)
 }
 
 app.use(express.static('public'))
+
+app.use('/',authUser,
+    function (req, res) {
+        res.redirect('/auctions');
+    }
+);
+app.use('/user',
+    authUser,
+    getUser,
+    renderMW
+);
+
+app.use('/user/edit',
+    authUser,
+    getUser,
+    editUser,
+    renderMW
+);
+
+app.use('/login',
+    reverseAuthUser,
+    validateUser,
+    renderMW
+);
+app.use('/logout',
+    authUser,
+    logoutUser,
+    function (req, res) {
+        res.redirect('/login');
+    }
+);
+
+app.use('/register',
+    reverseAuthUser,
+    checkUserRegistration,
+    addUser,
+    renderMW
+);
+
+app.use('/recover',
+    reverseAuthUser,
+    recoverPassword,
+    renderMW
+);
+
+app.use('/admin',
+    checkAdmin,
+    renderMW
+);
 
 app.get('/', function (req, res) {
     
@@ -34,4 +127,4 @@ app.get('/', function (req, res) {
     
 })
 
-app.listen(1337);
+app.listen(80);
