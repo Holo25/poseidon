2d7fee5fd353a29058707baf1edc7ef6c37e43a9
Holo25 <lorenzohorv@gmail.com>
2018-03-18 21:32:26 +0100

    Finished routes and middleware skeleton.

--- server.js
+++ server.js
@@ -17,57 +17,97 @@ var path = require('path');
 /auctions/:auctionid/bid
 
 
+finishAuction(), startAuction()
+Mivel az árverések befejezése egy belső esemény amit külső személy nem befolyásolhat, ezért ehhez nem készítettem külön route-t.
+Timer segítségével tervezem ezeket a függvényeket meghívni.
 */
-var userLoggedIn = true;
+var userLoggedIn = true;  //teszt valtozo
+
+var finishAuction = function(){
+    //A nyertesnek jóváírja a megvett tantárgyat majd törli a az árverést.
+}
+
+var startAuction = function(){
+    //Elkezd egy új árverést ha kevesebb számú van aktív mind a maximális megengedett
+}
 
 var authUser = function (req, res, next) {
-    //Visszaküld ha nem vagyunk  bejelentkezve
+    //Visszaküld a bejelentkező felületre ha nem vagyunk  bejelentkezve
     if (userLoggedIn) { next(); }
     else res.redirect('/login');
 }
 
 var reverseAuthUser = function (req, res, next) {
-    //Visszaküld ha be vagyunk  jelentkezve
+    //Visszaküld a főoldalra ha be vagyunk jelentkezve.
     if (!userLoggedIn) { next(); }
     else res.redirect('/');
 }
 var getUser = function (req, res, next) {
-    //megkeresi a jelenleg loginolt usert
+    //Megkeresi a jelenleg loginolt usert
+    next();
 }
 
 var editUser = function (req, res, next) {
-    //A változásokat elmenti a user profiljában 
+    //Elmenti a felhasználó adatait
+    next();
 }
 
 var addUser = function (req, res, next) {
     //Új usert készít 
+    next();
 }
 
 var validateUser = function (req, res, next) {
-    //Megnézi hogy ok a megadott bejelentkezési adatok
+    //Megnézi hogy OK a megadott bejelentkezési adatok
+    next();
 }
 
 var logoutUser = function (req, res, next) {
     //Kijelentkezteti a usert
+    next();
 }
 
 var checkUserRegistration = function (req, res, next) {
     //Megnézi hogy jók-e a regisztráló felhasználó adatai
+    next();
 }
 
 var recoverPassword = function (req, res, next) {
     //Elfelejtett jelszó middleware
+    next();
 }
 
 var checkAdmin = function (req, res, next) {
-    //Megnézi hogy admin-e a felhasználó
+    //Megnézi hogy admin-e a felhasználó, ha nem akkor visszadobja a főoldalra
+    if(true){
+        //admin vagyok
+        next();
+    }else res.redirect('/auctions');
 }
+
 var renderMW = function (req, res, next) {
     //Rendereli az adott oldalt
+    next();
 }
 
 var getAuctionList = function (req, res, next) {
     //Visszaadja az árverések listáját(aka főoldal tartalmát)
+    next();
+}
+
+var getAuction = function (req, res, next) {
+    //Visszaadja a megadott árverést
+    next();
+}
+
+var validateBid = function (req, res, next) {
+    //Érvényes-e a licitálás: Létezik az licitált árverés, van-e elég creditje hozzá és nagyobb árat adott meg mint a jelenlegi ár?
+    next();
+}
+
+var makeBid = function (req, res, next) {
+    //Frissíti az árverés jelenleg vezető userét és árát, jelenlegi usertől levon, előző usernek pedig visszaad crediteket.
+    next(); 
 }
 
 app.use(express.static('public'))
@@ -121,10 +161,24 @@ app.use('/admin',
     renderMW
 );
 
-app.get('/', function (req, res) {
+app.use('/auctions',
+    authUser,
+    getAuctionList,
+    renderMW
+);
+
+app.post('/auctions/bid',
+    authUser,
+    getAuction,
+    validateBid,
+    makeBid,
+    renderMW
+);
+
+/*app.get('/', function (req, res) {
     
      res.sendFile(path.join(__dirname + "/public/login.html"));
     
-})
+})*/
 
 app.listen(80);
